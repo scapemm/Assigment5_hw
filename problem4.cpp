bba70e80f140b780db74f624c495de7e056042d0
scapemm <a.shukurullayev@newuu.uz>
2024-11-08 12:29:03 +0500

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    int res=0;
+    for(int i=0; i<n; ++i) cin >> vec[i];
+    vector<int> rv(n);
+    for(int i=0; i<n; ++i){
+        rv[i]=vec[i];
+    }
+    reverse(rv.begin(),rv.end());
+    int ans=0;
+    for(int i=0; i<n/2; ++i){
+        for(int j=0; j<n; ++j){
+            if(rv[i]-vec[i] > ans) ans=rv[i]-vec[i];
+        }
+    }
+    cout << ans;
+    return 0;
+    
+}
